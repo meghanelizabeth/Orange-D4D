5c2ac83918c4ef45cb754815f38fe1423deb89a5
Meghan <meghanelizabethj@gmail.com>
2014-12-22 20:55:08 -0500

    delete useless code

--- set12v.R
+++ set12v.R
@@ -29,7 +29,8 @@ nrow(toubaIncoming2)
 
 grpIncomingCity2 <- group_by(toubaIncoming2, timestamp)
 grpincomingcity12 <- summarize(grpIncomingCity2, number_of_calls = sum(number_of_calls), total_call_duration = sum(total_call_duration))
-plot(grpincomingcity12$timestamp, grpincomingcity12$number_of_calls, main="Calls for Magal Dec 22,2013 with Touba as incoming city for calls(3rd)", xlab="Time", ylab="Nb calls")
+plot(grpincomingcity12$timestamp, grpincomingcity12$number_of_calls, main="Frequency of calls with Touba as receiving city", xlab="Time", ylab="Nb calls")
+points(grpincomingcity12[c(8),][1], grpincomingcity12[c(8),][2], col=2)
 
 #touba as outgoing city of origin 
 toubaOutgoing2 <- subset(set12v, outgoing_site_id == 1043 | outgoing_site_id == 1054 | outgoing_site_id == 1046 
@@ -38,8 +39,8 @@ toubaOutgoing2 <- subset(set12v, outgoing_site_id == 1043 | outgoing_site_id == 
 
 grpOutgoingCity2 <- group_by(toubaOutgoing2, timestamp)
 grpoutgoingcity12 <- summarize(grpOutgoingCity2, number_of_calls = sum(number_of_calls), total_call_duration = sum(total_call_duration))
-plot(grpoutgoingcity12$timestamp, grpoutgoingcity12$number_of_calls, main="Calls for Magal Dec 22,2013 with Touba as outcoming city for calls(3rd)", xlab="Time", ylab="Nb calls")
-
+plot(grpoutgoingcity12$timestamp, grpoutgoingcity12$number_of_calls, main="Frequency of calls with Touba as originating city", xlab="Time", ylab="Nb calls")
+points(grpoutgoingcity12[c(8),][1], grpoutgoingcity12[c(8),][2], col=2)
 
 
 #Dr Scharff's approach 1
@@ -70,6 +71,9 @@ where (outgoing_site_id = 1043 or outgoing_site_id = 1054 or outgoing_site_id = 
 group by timestamp")
 
 View(both)
-plot(both, main = "both")
+plot(both, main = "Frequency of calls with Touba as receiving and originating city")
+points(both[c(8),][1], both[c(8),][2], col=2)
+
+
 
 nrow(both)
